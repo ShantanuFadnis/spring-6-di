dd7620f35dd46961c0521178338964460b04cbb4
Shantanu Fadnis <shanfadnis@gmail.com>
2025-02-14 11:45:15 +0530

    Spring test context (#3)

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,13 +1,35 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController myController;
+
+    @Test
+    void testAutowireOfController() {
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromCtx() {
+        MyController controller = applicationContext.getBean(MyController.class);
+        assertEquals("Hello Everyone!!!", controller.sayHello());
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
