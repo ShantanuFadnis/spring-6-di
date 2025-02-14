7f1e9bc99ee0abd5c6279b3cd814365f8aa4e76c
Shantanu Fadnis <shanfadnis@gmail.com>
2025-02-14 17:39:59 +0530

    Spring profiles and defautl, ActiveProfile and Profile (#8)

---
+++ src/test/java/guru/springframework/spring6di/controllers/i18n/Myi18NControllerTestES.java
@@ -0,0 +1,18 @@
+package guru.springframework.spring6di.controllers.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@ActiveProfiles("ES") -- when there is no profile available then use "default" profile
+@SpringBootTest
+class Myi18NControllerTestES {
+
+    @Autowired
+    Myi18NController myi18NController;
+
+    @Test
+    void sayHello() {
+        System.out.println(myi18NController.sayHello());
+    }
+}
